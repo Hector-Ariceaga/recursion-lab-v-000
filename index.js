8252fa8f3d757bc61582eae030d381cda31997ae
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-03-15 15:39:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 // Code your solution here!
 
+function printString(string) {
+  console.log(string[0])
+  let remainder = string.substring(1, string.length)
+
+  if (string.length > 1) {
+    printString(remainder)
+  } else {
+    return true
+  }
+}
+
+function reverseString(string) {
+  if (string) {
+    reversedString.unshift(string.slice(0,-1))
+    reverseString(string.slice(0,-1))
+  } else {
+    return reversedString.join('')
+  }
+}
+
+
