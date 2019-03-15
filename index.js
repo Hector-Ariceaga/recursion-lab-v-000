14b03dc44a393747744622a5c0ba818913613fcf
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-03-15 16:35:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -42,6 +42,6 @@ function maxOf(array) {
   if (array.length === 1) {
     return array
   } else {
-    return Math.max(array.splice(0,1), maxOf(array))
+    return Math.max(array.unshift(), maxOf(array))
   }
 }
