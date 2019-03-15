d04952ab4b11906d9cd082def9b88f59251b41f5
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-03-15 16:22:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -39,14 +39,14 @@ function addUpTo(array, index) {
 
 function maxOf(array) {
   console.log(array)
-  if (array.length > 1) {
+  if (array.length === 1) {
+    return array
+  } else {
     if (array[0] > array[1]) {
       console.log(array[0], array[1])
       return maxOf(array.splice(1,1))
     } else {
       return maxOf(array.splice(0,1))
     }
-  } else {
-    return array
   }
 }
